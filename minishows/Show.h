9317fe9dff01cfbb83d83ae3d4db780a0a6d8f78
Francisco Caro Diaz <franciscocarodiaz@gmail.com>
2014-10-21 18:40:15 +0200

    Object class for MiniShow.

---
+++ minishows/Show.h
@@ -0,0 +1,30 @@
+//
+//  Show.h
+//  minishows
+//
+//  Created by Francisco Caro Diaz on 21/10/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+
+@interface Show : NSObject
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *episode;
+@property (nonatomic) BOOL watched;
+@property (nonatomic, strong) NSString *generalInfo;
+@property (nonatomic, strong) NSString *overview;
+@property (nonatomic, strong) NSString *image;
+
+- (void) prettyPrint;
++(Show *)showWithName:(NSString *)name;
++(Show *)show;
+
+- (instancetype)initWithName:(NSString *)name;
+- (instancetype)initWithName:(NSString *)name andEpisode:(NSString *)episode;
+- (instancetype)initWithName:(NSString *)name andEpisode:(NSString *)episode andGeneralInfo:(NSString *)generalInfo;
+- (instancetype)initWithName:(NSString *)name andEpisode:(NSString *)episode andGeneralInfo:(NSString *)generalInfo andOverview:(NSString *)overview andImage:(NSString *)image;
+
+@end
---
+++ minishows/Show.m
@@ -0,0 +1,13 @@
+//
+//  Show.m
+//  minishows
+//
+//  Created by Francisco Caro Diaz on 21/10/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import "Show.h"
+
+@implementation Show
+
+@end
